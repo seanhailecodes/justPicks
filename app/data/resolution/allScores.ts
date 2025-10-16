bf664451165f828fb06d253dc506f5d1ad6a57ce
Sean Haile <seanhaile.codes@gmail.com>
2025-10-16 16:30:56 -0400

    resolved week 6 finals and updated home screen to reflect week 7 in ui

--- app/data/resolution/allScores.ts
+++ app/data/resolution/allScores.ts
@@ -5,20 +5,20 @@ import { GameScore } from './week1-scores-2025';
 import { WEEK_1_SCORES_2025 } from './week1-scores-2025';
 import { WEEK_2_SCORES_2025 } from './week2-scores-2025';
 import { WEEK_5_SCORES_2025 } from './week5-scores-2025';
+import { WEEK_6_SCORES_2025 } from './week6-scores-2025';
 // Uncomment when files are created:
 // import { WEEK_3_SCORES_2025 } from './week3-scores-2025';
 // import { WEEK_4_SCORES_2025 } from './week4-scores-2025';
-// import { WEEK_6_SCORES_2025 } from './week6-scores-2025';
 
 // Registry for automatic lookup
 export const ALL_SCORES_REGISTRY: Record<number, GameScore[]> = {
   1: WEEK_1_SCORES_2025,
   2: WEEK_2_SCORES_2025,
   5: WEEK_5_SCORES_2025,
+  6: WEEK_6_SCORES_2025,
   // Uncomment as you add scores:
   // 3: WEEK_3_SCORES_2025,
   // 4: WEEK_4_SCORES_2025,
-  // 6: WEEK_6_SCORES_2025,
 };
 
 // Get scores for a specific week
