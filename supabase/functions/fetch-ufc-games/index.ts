5af173f4d3d42d579aac9326c1d806cd91f2048e
Sean <seanhaile.codes@gmail.com>
2026-05-31 03:50:38 +0000

    ufc/boxing: note unpriced skip in upsert log

--- supabase/functions/fetch-ufc-games/index.ts
+++ supabase/functions/fetch-ufc-games/index.ts
@@ -119,7 +119,7 @@ Deno.serve(async (req) => {
 
     if (error) throw error;
 
-    console.log(`Upserted ${upsertable.length} UFC fights (skipped ${games.length - upsertable.length} locked)`);
+    console.log(`Upserted ${upsertable.length} UFC fights (skipped ${games.length - upsertable.length} locked/unpriced)`);
 
     await mergeDuplicateGames(supabase, "UFC", games);
 
